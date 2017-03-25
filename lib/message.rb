92a73d13701b4bcde04b5036e5534332b538c6f6
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-03-25 10:47:27 -0600

    functional message class

--- lib/message.rb
+++ lib/message.rb
@@ -1,10 +1,27 @@
 require 'pry'
-require 'encryptor'
+require './lib/encryptor'
 
 class Message
 
-  def initialize
-
+  def initialize(message="")
+    @message = message
   end
+
+  def read_file
+    @message = File.read(@message).gsub("\n", " ")
+  end
+  
+  def parse
+    @message = @message.split('')
+  end
+  
+  def split_into_sub_arrays
+    @message = @message.each_slice(4).to_a
+  end
+  
+  def write_file(file_name)
+    File.write(file_name, @message)
+  end
+
   
 end
