e0fba3923d8c596ff4f3ab15eab9a9d168ca242e
rongxanh88 <rongxanh88@gmail.com>
2017-03-27 13:35:32 -0600

    change encryption modes to symbol

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -8,13 +8,13 @@ class Enigma
 
   def encrypt(message=@message, key=@key, date=Date.today)
     e = Cryptor.new(message, key, date)
-    output = e.crypt("encrypt")
+    output = e.crypt(:ENCRYPT)
     p "message was encrypted with key #{e.key}. Encrypted message: #{output}"
   end
 
   def decrypt(message=@message, key=@key, date=Date.today)
     e = Cryptor.new(message, key, date)
-    e.crypt("decrypt")
+    e.crypt(:DECRYPT)
   end
 
   def crack(message=@message, date=Date.today)
