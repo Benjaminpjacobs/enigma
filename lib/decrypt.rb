b5a75ac10be3a8ecbb161508c3af02af980d2b7c
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-03-27 16:53:44 -0600

    command line args for encrypt and decrypt

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,16 +2,21 @@ require './lib/cryptor'
 require './lib/message_io'
 require 'pry'
 
-class Encrypt
+class Decrypt
   def initialize(input=nil, output=nil, key=nil, date=nil)
     @input = input
     @output = output
     @key = key
-    @date = date
-    @mode = :ENCRYPT
+    @date = convert_date(date) unless date.nil?
+    @mode = :DECRYPT
   end
-  
-  def encrypt
+
+  def convert_date(date)
+    date_split = date.split('').each_slice(2).to_a.map{|sub| sub.join}
+    date = Date.new(("20"+ date_split[2].to_s).to_i,date_split[1].to_i, date_split[0].to_i)
+  end
+
+  def decrypt
     messenger = MessageIO.new(@input)
     @input = messenger.read_file
     e = Cryptor.new(@input, @key, @date)
@@ -19,10 +24,10 @@ class Encrypt
     messenger.write_file(@output, message)
     p "Created #{@output} with key of #{e.key} and date #{e.date}"
   end
-  
+
 end
 
-###########################################
+####################################
 
-#e = Encrypt.new(ARGV[0], ARGV[1])
-#e.encrypt
+# d = Decrypt.new(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
+# d.decrypt
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -1,5 +1,6 @@
 require './test/test_helper.rb'
 require './lib/encrypt'
+require 'pry'
 
 class EncryptTest < Minitest::Test
   def test_it_exists
@@ -9,8 +10,15 @@ class EncryptTest < Minitest::Test
   def test_it_encrypt_a_message
     e = Encrypt.new("./test/single_line.txt", "./test/encryptor_message.txt" )
     e.encrypt
-    # assert File.read("./test/encryptor_message.txt")
     assert File.delete("./test/encryptor_message.txt")
+  end
+
+  def test_it_can_encrypt_with_key_and_date
+    e = Encrypt.new("./test/single_line.txt","./test/encrypted_message1.txt", 12345, Date.new(2017, 03, 27))
+    e.encrypt
+    expected = "^8L*J9VgmYF$p5GiJ7LQn5UX48>@qGVPs5"
+    actual = File.read("./test/encrypted_message1.txt")
+    assert_equal expected, actual
   end
 
 end
